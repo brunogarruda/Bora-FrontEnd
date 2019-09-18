63446073f1df994c28abf2fd9a9c1ed7f7dce830
tjambeiro <thiago_jamb@hotmail.com>
2019-09-18 18:00:59 -0300

    Inclusão do Component Evento e Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,22 @@
 import React,{Component} from 'react';
+import Eventos from '../../component/Eventos'
 
 
 class Home extends Component{
-    constructor(props){
-        super(props);
-        // this.state = {
+//     constructor(props){
+//         super(props);
+//         // this.state = {
 
-        // }
-    }
+//         // }
+//     }
 
     render(){
         return(
             <div>
-
+                <Eventos />
             </div>
         );
     }
-} 
+}
 
 export default Home;
