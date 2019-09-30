fb456167afd84be2299b132ad12dd488fa65a11b
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-30 20:31:14 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,24 +14,18 @@ class Footer extends Component{
         return(
             <div>
                 <footer>
-                    <div className="footer-item borda-teste">
-                        <div className="logo-rodape borda-teste">
+                    <div className="footer-item">
+                        <div className="logo-rodape">
                             <p>BORA</p>
                         </div>
-                        <p>FALE COM A GENTE</p>
-                        <div className="redes-sociais-icons borda-teste">
+                        <p>Fale com a gente</p>
+                        <div className="redes-sociais-icons">
                             <img  />
                             <img />
                             <img />
                         </div>
                     </div>
-                    <div className="footer-item borda-teste">
-                        <div className="newsletter borda-teste">
-                            <p className="borda-teste">Newsletter</p>
-                            <p className="borda-teste">Assine nossa newsletter e fique sabendo dos rolês</p>
-                            <input type="text" name="newsletter" value="" placeholder="Digite seu e-mail" />
-                        </div>
-                    </div>
+                    
 
                 </footer>
             </div>
