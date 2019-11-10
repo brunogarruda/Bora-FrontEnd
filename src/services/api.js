317251e237761f9f5ff2d5bb882304e73550c6db
tjambeiro <thiago_jamb@hotmail.com>
2019-11-10 20:44:57 -0300

    inclusão dos banners nas telas de editar e detalhe

--- src/services/api.js
+++ src/services/api.js
@@ -4,15 +4,16 @@ import { getToken } from "./auth";
 
 export const api = axios.create({
   baseURL: "http://localhost:8080/api/",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    "Access-Control-Allow-Credentials": true,
-    "crossorigin": true,
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-  }
 });
+
+// headers: {
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+//   "Access-Control-Allow-Credentials": true,
+//   "crossorigin": true,
+//   "Accept": "application/json",
+//   "Content-Type": "application/json"
+// }
 
 // api.interceptors.request.use(async config => {
 //   const token = getToken();
