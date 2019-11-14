99ff2596a5b9128b9f5ebd225d613dfafb59ba82
tjambeiro <thiago_jamb@hotmail.com>
2019-11-13 21:40:24 -0300

    Alterações William

--- src/pages/EditarEventos/index.js
+++ src/pages/EditarEventos/index.js
@@ -1,8 +1,7 @@
-import React, {Component} from 'react';
-import banner from '../../assets/bg-editarEvento.png';
-import { api } from '../../services/api';
-import './style.css';
-
+import React, {Component} from "react";
+import banner from "../../assets/bg-editarEvento.png";
+import { api } from "../../services/api";
+import "./style.css";
 
 export default class EditarEventos extends Component{
     constructor(props){
@@ -12,6 +11,7 @@ export default class EditarEventos extends Component{
             endereco: []
         };
     }
+
     componentDidMount() {
         const { id } = this.props.match.params;
         try {
@@ -31,30 +31,45 @@ export default class EditarEventos extends Component{
     render(){
         return(
             <>
-            <img src={banner}/>
-                <h2>Editar o Evento</h2>
+            <img src={banner} />
+                
                 <div className="container">
+                <h2 className="title-form"><b>Editar informações do evento</b></h2>
                     <form>
                         <div className="form-group">
-                            <label htmlFor="">Nome do evento</label>
-                            <input type="text" className="form-control" placeholder="Inclua um nome curto e claro" />
-                            <label htmlFor="">Localização</label>
-                            <input type="text" className="form-control" placeholder="Inclua o endereço do evento" />
-                            <label htmlFor="">Descrição</label>
-                            <textarea type="text" className="form-control" placeholder="Inclua o endereço do evento" rows="3" />
-                            <label>Categoria</label>
-                            <select className="custom-select">
-                                <option>Selecione uma categoria</option>
-                            </select>
-                            <label>Frequencia</label>
-                            <select className="custom-select">
-                                <option>Ocorre uma vez</option>
-                            </select>
+                            <div className="groupinput">
+                                <label className="labelForm" htmlFor="">Nome do evento</label>
+                                <input className="inputForm" type="text" className="form-control" placeholder="Inclua um nome curto e claro" />
+                            </div>
+                            <div className="groupinput">
+                                <label className="labelForm" htmlFor="">Localização</label>
+                                <input className="inputForm" type="text" className="form-control" placeholder="Inclua o endereço do evento" />
+                            </div>
+                            <div className="groupinput">
+                                <label className="labelForm" htmlFor="">Descrição</label>
+                                <textarea className="inputForm" type="text" className="form-control" placeholder="Inclua o endereço do evento" rows="3" />
+                            </div>
+                            <div className="groupinput">
+                                <label className="labelForm">Categoria</label>
+                                <select className="inputForm" className="custom-select">
+                                    <option>Selecione uma categoria</option>
+                                </select>
+                            </div>
+                            <div className="groupinput">
+                                <label className="labelForm">Frequencia</label>
+                                <select className="inputForm" className="custom-select">
+                                    <option>Ocorre uma vez</option>
+                                </select>
+                            </div>
                         </div>
-
+                        <div className="groupinput groupinputButton">
+                            <button className="botao-deixa-pra-la">Não Vou</button>
+                            <button className="botao-pronto">Pronto</button>
+                        </div>
+                       
                     </form>
                 </div>
             </>
-        )
+        );
     }
 }
