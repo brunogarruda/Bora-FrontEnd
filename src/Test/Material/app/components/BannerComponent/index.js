1b82c14eacd15d9c2caf00c241dd46f59258877e
GBernardo10 <g_bernardo10@hotmail.com>
2019-11-27 00:19:04 -0300

    cria banner-material

--- src/Test/Material/app/components/BannerComponent/index.js
+++ src/Test/Material/app/components/BannerComponent/index.js
@@ -1,27 +1,72 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/alt-text */
-import React from "react";
+import { Box, Fab, Grid, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
-
-import bg from "../../../../../assets/bg.svg";
+import { AccountCircle } from "@material-ui/icons";
+import React from "react";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex",
-    flexWrap: "wrap",
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary
   },
-  imageSrc:{
+  img: {
+    width: "100%"
+  },
+  imageTitle: {
+    position: "relative",
+    padding: `${theme.spacing(1)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`
+  },
+  imageButton: {
     position: "absolute",
-    backgroundSize: "cover",
     left: 0,
     right: 0,
-    backgroundPosition: "center 40%"
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    justifyContent: "center",
+    color: theme.palette.common.white
+  },
+  image: {
+    position: "relative",
+    height: "auto"
+  },
+  bg: {
+    backgroundImage: "url(https://svgshare.com/i/GQQ.svg)",
+    backgroundSize: "cover",
+    width: "100%",
+    height: 768,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 0
+  },
+  btn1: {
+    position: "relative",
+    right: "20%"
+  },
+  btn2: {
+    position: "relative",
+    left: "20%"
+  },
+  bannerTitulo: {
+    position: "absolute",
+    top: "15%",
+    color: "white",
+    padding: "4%",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "4em"
+    }
+  },
+  cardEventoHome: {
+    height: 400,
+    width: 300,
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      width: 200
+    }
   }
 }));
 
@@ -29,12 +74,55 @@ export const Banner = () => {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <img className={classes.imageSrc} style={{ width: `${100}%` }} src={bg} />
-
-      </Grid>
+    <Grid container>
+      <Paper className={classes.bg}>
+        <Typography color="#2196f3" style={{ display: "contents" }}>
+          <Box fontWeight="fontWeightBold" fontSize="5em" className={classes.bannerTitulo}>
+            O que voce quer fazer ?
+          </Box>
+        </Typography>
+        <section style={{ display: "flex" }}>
+          <Fab
+            className={classes.btn1}
+            style={{ width: 100 }}
+            variant="extended"
+            size="medium"
+            color="primary"
+            aria-label="add"
+          >
+            <span className={classes.imageButton}>
+              <Typography
+                component="span"
+                variant="subtitle1"
+                color="inherit"
+                className={classes.imageTitle}
+              >
+                <Box fontSize={15}>Bora</Box>
+              </Typography>
+            </span>
+          </Fab>
+          <Fab
+            className={classes.btn2}
+            style={{ width: 150 }}
+            variant="extended"
+            size="medium"
+            color="primary"
+            aria-label="add"
+          >
+            <span className={classes.imageButton}>
+              <Typography
+                component="span"
+                variant="subtitle1"
+                color="inherit"
+                className={classes.imageTitle}
+                noWrap
+              >
+                <Box fontSize={15}>Criar role</Box>
+              </Typography>
+            </span>
+          </Fab>
+        </section>
+      </Paper>
     </Grid>
   );
 };
-
